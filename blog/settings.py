5c86cba707c256ffaee824b379aeedbbab2bfb60
Brenda <brentorres26@gmail.com>
2025-12-08 11:08:23 -0300

    Configuración BD mysql

--- blog/settings.py
+++ blog/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -96,6 +97,19 @@ DATABASES = {
     #    "ENGINE": "django.db.backends.sqlite3",
    #     "NAME": BASE_DIR / "db.sqlite3",
    # }
+
+   # Usando MySql
+   'ENGINE': 'django.db.backends.mysql',
+
+   'NAME': os.getenv('DB_NAME'),
+
+   'USER': os.getenv('DB_USER'),
+
+   'PASSWORD': os.getenv('DB_PASSWORD'),
+
+   'HOST': os.getenv('DB_HOST'),
+
+   'PORT': os.getenv('DB_PORT'),
 }
 
 
